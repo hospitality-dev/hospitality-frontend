870b6048e94c0a8c94a66b68706319f2b57e2c96
sergio <srdjanljiljak10@gmail.com>
2025-02-09 14:18:57 +0100

    chore: add dummy buttons

--- packages/hospitality-ui/src/App.tsx
+++ packages/hospitality-ui/src/App.tsx
@@ -1,7 +1,16 @@
+import Button from "./components/Button";
+
 function App() {
   return (
-    <main></main>
-  )
+    <main>
+      <Button label="Stagod" variant="primary" />
+      <Button label="Stagod" variant="secondary" />
+      <Button label="Stagod" variant="success" />
+      <Button label="Stagod" variant="info" />
+      <Button label="Stagod" variant="error" />
+      <Button label="Stagod" variant="warning" />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- packages/hospitality-ui/src/components/Button.tsx
+++ packages/hospitality-ui/src/components/Button.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { tv } from "tailwind-variants";
+import { Size, Variant } from "../types/baseTypes";
 
-type Props = {};
+type Props = { label?: string; variant?: Variant; size?: Size };
 
-export default function Button({}: Props) {
-  return <button>Button</button>;
+const classes = tv({
+  base: "shadow active:shadow-none rounded-md",
+  variants: {
+    variant: {
+      primary: "bg-gray-700 hover:bg-gray-900 active:bg-gray-900",
+      secondary: "bg-gray-300 hover:bg-gray-600 active:bg-gray-600",
+      info: "bg-blue-500 hover:bg-blue-600  active:bg-blue-600 ",
+      success: "bg-green-500 hover:bg-green-600  active:bg-green-600",
+      warning: "bg-orange-500 hover:bg-orange-600  active:bg-orange-600",
+      error: "bg-red-600 hover:bg-red-800  active:bg-red-800",
+    },
+    size: { xs: "", sm: "", md: "", lg: "", xl: "" },
+  },
+});
+
+export default function Button({
+  label,
+  variant = "primary",
+  size = "md",
+}: Props) {
+  return <button className={classes({ variant })}>{label}</button>;
 }
