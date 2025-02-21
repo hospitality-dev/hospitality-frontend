ac48c19ddd1254751db58ed771f3053f3eda5860
Mihailo <mihailo.dilparic@gmail.com>
2025-02-21 15:08:14 +0100

    feat: initial auth hooks

--- packages/hospitality-ui/src/index.ts
+++ packages/hospitality-ui/src/index.ts
@@ -2,3 +2,5 @@ export * from "./components";
 export * from "./enums";
 export * from "./hooks";
 export * from "./types";
+export { useForm } from "@tanstack/react-form";
+export { QueryClient, QueryClientProvider } from "@tanstack/react-query";
