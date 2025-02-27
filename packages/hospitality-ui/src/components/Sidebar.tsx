949953b240e4f699ccf3f27c599f23975f04f45e
Mihailo <mihailo.dilparic@gmail.com>
2025-02-27 15:46:06 +0100

    feat: sidebar component

---
+++ packages/hospitality-ui/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+export function Sidebar() {
+  return (
+    <div className="w-52 bg-blue-900 p-2 text-white">
+      <ul>
+        <li>LOGO</li>
+        <li>Locations V</li>
+        <li>Apps V</li>
+      </ul>
+    </div>
+  );
+}
