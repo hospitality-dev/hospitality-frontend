4580105cbecb1cd1ece55834205ee2850351683b
Mihailo <mihailo.dilparic@gmail.com>
2025-03-01 11:19:02 +0100

    feat: custom zod schemas

--- packages/hospitality-ui/src/index.ts
+++ packages/hospitality-ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./components";
 export * from "./enums";
 export * from "./hooks";
+export * from "./schemas";
 export * from "./types";
 export { type UpdaterFn, useForm } from "@tanstack/react-form";
 export { QueryClient, QueryClientProvider } from "@tanstack/react-query";
---
+++ packages/hospitality-ui/src/schemas/auth/login.ts
@@ -0,0 +1,14 @@
+import { literal, object, string } from "zod";
+
+import { users } from "../../types";
+
+export const loginParamsSchema = object({
+  username: string().nonempty("Username cannot be empty."),
+  password: string().nonempty("Password cannot be empty."),
+});
+
+export const loginResponseSchema = object({
+  message: literal("Success."),
+  ok: literal(true),
+  data: users.pick({ id: true, firstName: true, lastName: true, username: true, phone: true, email: true }),
+});
