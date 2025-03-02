1d939de6264f60fa2355bda677db4587055907be
Mihailo <mihailo.dilparic@gmail.com>
2025-03-02 20:07:44 +0100

    feat: global generic fetch fn

--- packages/hospitality-ui/src/utils/request.ts
+++ packages/hospitality-ui/src/utils/request.ts
@@ -1,4 +1,8 @@
+import ky from "ky";
 import snakecase from "lodash.snakecase";
+
+import { AvailableEntities, ResponseType } from "../types";
+import { formatDataResponseHook } from "./response";
 
 export function getSearchParams<F, R>(fields: F, relations: R) {
   const searchParams = new URLSearchParams();
@@ -6,9 +10,35 @@ export function getSearchParams<F, R>(fields: F, relations: R) {
   if (Array.isArray(fields)) {
     searchParams.append("fields", fields.map((f) => (typeof f === "string" ? snakecase(f) : f)).join(","));
   }
-  if (Array.isArray(relations)) {
+  if (Array.isArray(relations) && relations.length) {
     searchParams.append("relations", relations.map((f) => (typeof f === "string" ? snakecase(f) : f)).join(","));
   }
 
   return searchParams;
+}
+
+export async function fetchFunction<DataType>({
+  method = "GET",
+  model,
+  id,
+  searchParams,
+  userReset,
+}: {
+  method?: "GET" | "POST" | "PATCH" | "DELETE";
+  model: AvailableEntities;
+  id: string;
+  searchParams: URLSearchParams;
+  userReset: () => void;
+}): Promise<DataType> {
+  const result = await ky(`${model}/${id}`, {
+    method,
+    searchParams,
+    prefixUrl: `${import.meta.env.VITE_SERVER_URL}/api/v1`,
+    credentials: "include",
+    hooks: {
+      afterResponse: [(_, __, res) => formatDataResponseHook(_, __, res, userReset)],
+    },
+  }).json<ResponseType<DataType>>();
+
+  return result?.data;
 }
