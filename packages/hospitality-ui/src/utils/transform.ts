f94c5431b08abdefa5ae884006fd7382becad2d1
Mihailo <mihailo.dilparic@gmail.com>
2025-03-03 15:36:12 +0100

    fix: regex

--- packages/hospitality-ui/src/utils/transform.ts
+++ packages/hospitality-ui/src/utils/transform.ts
@@ -1,4 +1,4 @@
 export function getSentenceCase(field: string) {
-  const text = field?.replaceAll("_", " ")?.replace(/([A-Z])/g, " $1") || "";
+  const text = field?.replaceAll(/[_-]/g, " ")?.replace(/([A-Z])/g, " $1") || "";
   return `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
 }
