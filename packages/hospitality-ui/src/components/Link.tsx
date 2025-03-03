9487db9107ebcbcfc739b2653ea847a0a199af9e
Mihailo <mihailo.dilparic@gmail.com>
2025-03-03 11:55:39 +0100

    feat: Link component

---
+++ packages/hospitality-ui/src/components/Link.tsx
@@ -0,0 +1,40 @@
+import { createLink, LinkOptions } from "@tanstack/react-router";
+import * as React from "react";
+import { tv } from "tailwind-variants";
+
+import { Variant } from "../types";
+
+const classes = tv({
+  base: "inline cursor-pointer transition-colors",
+  variants: {
+    variant: {
+      primary: "text-gray-900 hover:text-gray-400 active:text-gray-950",
+      secondary: "text-gray-300 hover:text-gray-200 active:text-gray-500",
+      info: "text-blue-500 hover:text-blue-300 active:text-blue-700",
+      success: "text-green-500 hover:text-green-300 active:text-green-700",
+      warning: "text-orange-500 hover:text-orange-300 active:text-orange-700",
+      error: "text-red-600 hover:text-red-400 active:text-red-800",
+    },
+    isDisabled: {
+      true: "cursor-not-allowed text-gray-200",
+    },
+  },
+});
+
+interface BasicLinkProps extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "target">, LinkOptions {
+  isDisabled?: boolean;
+  variant?: Variant;
+}
+
+function BasicLinkComponent(props: BasicLinkProps, ref: React.Ref<HTMLAnchorElement>) {
+  return (
+    <a ref={ref} {...props} className={classes({ variant: props.variant || "primary", isDisabled: !!props?.isDisabled })} />
+  );
+}
+
+const ForwardedBasicLinkComponent = React.forwardRef(BasicLinkComponent);
+const CreatedLinkComponent = createLink(ForwardedBasicLinkComponent);
+
+export function Link(props: Parameters<typeof BasicLinkComponent>[0]) {
+  return <CreatedLinkComponent {...props} />;
+}
--- packages/hospitality-ui/src/components/index.ts
+++ packages/hospitality-ui/src/components/index.ts
@@ -2,5 +2,6 @@ export * from "./Button";
 export * from "./Checkbox";
 export * from "./Form";
 export * from "./Input";
+export * from "./Link";
 export * from "./Navbar";
 export * from "./Sidebar";
