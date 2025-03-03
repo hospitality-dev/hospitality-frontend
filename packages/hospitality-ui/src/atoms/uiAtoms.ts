b59707460b6ff575dd64e40ee318f03c3784f9d6
Mihailo <mihailo.dilparic@gmail.com>
2025-03-03 15:23:10 +0100

    feat: sidebar state atom

---
+++ packages/hospitality-ui/src/atoms/uiAtoms.ts
@@ -0,0 +1,6 @@
+import { atom } from "jotai";
+type SidebarState = {
+  isSidebarOpen: boolean;
+  isModulesOpen: boolean;
+};
+export const sidebarStateAtom = atom<SidebarState>({ isSidebarOpen: false, isModulesOpen: false });
