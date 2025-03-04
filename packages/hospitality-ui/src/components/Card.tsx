ea2e2d6b0d1ea9f242d77c9fd30ab5f2f71c1d98
Mihailo <mihailo.dilparic@gmail.com>
2025-03-04 09:59:38 +0100

    feat: variants for card

--- packages/hospitality-ui/src/components/Card.tsx
+++ packages/hospitality-ui/src/components/Card.tsx
@@ -1,12 +1,32 @@
 import { ReactNode } from "react";
+import { tv } from "tailwind-variants";
+
+import { Variant } from "../types";
 
 type Props = {
   children: ReactNode;
+  isFullWidth?: boolean;
+  variant?: Variant;
 };
-export function Card({ children }: Props) {
-  return (
-    <div className="aspect-square w-fit min-w-32 cursor-pointer rounded bg-white shadow-sm transition-shadow hover:shadow-md">
-      {children}
-    </div>
-  );
+
+const classes = tv({
+  base: "cursor-pointer overflow-hidden rounded shadow-sm transition-shadow hover:shadow-md",
+  variants: {
+    isFullWidth: {
+      true: "w-full",
+      false: "aspect-square w-fit min-w-32",
+    },
+    variant: {
+      primary: "bg-white",
+      secondary: "bg-gray-300",
+      info: "border-3 border-blue-500",
+      success: "border-3 border-green-500",
+      warning: "border-3 border-orange-500",
+      error: "border-3 border-red-500",
+    },
+  },
+});
+
+export function Card({ children, isFullWidth, variant = "primary" }: Props) {
+  return <div className={classes({ isFullWidth, variant })}>{children}</div>;
 }
