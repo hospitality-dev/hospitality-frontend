1fb4979e0800ceae974c5a3307067a5a7ceb3dce
Mihailo <mihailo.dilparic@gmail.com>
2025-03-04 10:27:21 +0100

    feat: inital sections and layout padding

--- apps/hospitality-shell/src/components/Layout.tsx
+++ apps/hospitality-shell/src/components/Layout.tsx
@@ -1,12 +1,22 @@
-import { Navbar, Outlet, Sidebar, TanStackRouterDevtools } from "@hospitality/hospitality-ui";
+import { getSentenceCase, Navbar, Outlet, Sidebar, TanStackRouterDevtools } from "@hospitality/hospitality-ui";
+import { AvailableModules } from "../enums";
+
+const sections = [
+  {
+    title: "MODULES",
+    links: AvailableModules.map((module) => ({ title: getSentenceCase(module), to: `${module}/dashboard` })),
+  },
+];
 
 export function Layout() {
   return (
     <main className="flex h-screen w-screen flex-nowrap overflow-hidden bg-gray-200">
-      <Sidebar />
+      <Sidebar sections={sections} />
       <div className="flex w-full flex-col">
         <Navbar />
-        <Outlet />
+        <div className="p-4">
+          <Outlet />
+        </div>
       </div>
       <TanStackRouterDevtools />
     </main>
