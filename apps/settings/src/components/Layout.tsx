331616bc536a1c35a9c5f2f3faf6b957b2f7d0b0
Mihailo <mihailo.dilparic@gmail.com>
2025-03-05 13:18:07 +0100

    feat: tabs component

--- apps/settings/src/components/Layout.tsx
+++ apps/settings/src/components/Layout.tsx
@@ -1,15 +1,11 @@
-import { Outlet } from "@hospitality/hospitality-ui";
+import { Outlet, Tabs } from "@hospitality/hospitality-ui";
+
+const tabs = [{ title: "Users", link: "/settings/users" }, { title: "Locations" }, { title: "Products" }];
 
 export function SettingsLayout() {
   return (
     <div className="flex flex-col gap-y-2">
-      <nav>
-        <ul>
-          <li>Locations</li>
-          <li>Products</li>
-          <li>Users</li>
-        </ul>
-      </nav>
+      <Tabs tabs={tabs} />
       <Outlet />
     </div>
   );
