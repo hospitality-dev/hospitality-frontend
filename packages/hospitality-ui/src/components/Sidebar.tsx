9c9c7115686449e1bc0fcddb49486aa8271bf4ef
Mihailo <mihailo.dilparic@gmail.com>
2025-03-05 11:45:56 +0100

    feat: settings link in sidebar

--- packages/hospitality-ui/src/components/Sidebar.tsx
+++ packages/hospitality-ui/src/components/Sidebar.tsx
@@ -12,10 +12,21 @@ type SidebarSectionType = {
 };
 type Props = { sections: SidebarSectionType[] };
 
+function SidebarLink({ title, to }: SidebarSectionType["links"][number]) {
+  return (
+    <Link
+      className="h-fit w-full px-4 py-2 font-semibold duration-0 select-none hover:bg-white hover:text-gray-900"
+      to={to}
+      variant="secondary">
+      {title}
+    </Link>
+  );
+}
+
 function SidebarSection({ title, links }: SidebarSectionType) {
   const [sidebarState, setSidebarState] = useAtom(sidebarStateAtom);
   return (
-    <li className="flex flex-col gap-y-2 border-y border-gray-500 first:pt-4 last:pb-4">
+    <li className="flex flex-col gap-y-2 border-y border-gray-500 first:pt-2 last:pb-2">
       <h3
         className="flex cursor-pointer items-center justify-between border-gray-100 px-4 py-1 text-sm font-bold"
         onClick={() => setSidebarState((prev) => ({ ...prev, isModulesOpen: !sidebarState.isModulesOpen }))}>
@@ -25,13 +36,7 @@ function SidebarSection({ title, links }: SidebarSectionType) {
       <div
         className={`flex flex-col gap-y-1 ${sidebarState.isModulesOpen ? "h-auto" : "h-0"} overflow-hidden transition-[height]`}>
         {links.map((link) => (
-          <Link
-            key={link.to}
-            className="h-fit px-4 py-2 font-semibold duration-0 select-none hover:bg-white hover:text-gray-900"
-            to={link.to}
-            variant="secondary">
-            {link.title}
-          </Link>
+          <SidebarLink key={link.to} title={link.title} to={link.to} />
         ))}
       </div>
     </li>
@@ -41,7 +46,7 @@ function SidebarSection({ title, links }: SidebarSectionType) {
 export function Sidebar({ sections = [] }: Props) {
   return (
     <div className="w-64 bg-blue-950 text-white">
-      <ul>
+      <ul className="flex h-full flex-col">
         <li className="flex h-16 flex-nowrap items-center gap-x-4 px-4">
           <div className="flex h-10 w-10 items-center justify-center rounded-full border-blue-300 bg-blue-500 text-sm">
             LOGO
@@ -53,6 +58,9 @@ export function Sidebar({ sections = [] }: Props) {
             <SidebarSection key={section.title} links={section.links} title={section.title} />
           ))}
         </ul>
+        <li className="mt-auto mb-4 flex w-full">
+          <SidebarLink title="Settings" to="/settings/users" />
+        </li>
       </ul>
     </div>
   );
