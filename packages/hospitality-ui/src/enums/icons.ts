3baae96d2c36dfbb2995f98bf671ad2888553894
Mihailo <mihailo.dilparic@gmail.com>
2025-03-07 14:29:30 +0100

    feat: new icon

--- packages/hospitality-ui/src/enums/icons.ts
+++ packages/hospitality-ui/src/enums/icons.ts
@@ -1,6 +1,7 @@
 export const Icons = {
   add: "ph:plus" as const,
   "arrow-down": "ph:caret-down" as const,
+  "arrow-right": "ph:caret-right" as const,
   "arrow-up": "ph:caret-up" as const,
   beer: "ph:beer-bottle-light" as const,
   chicken: "healthicons:animal-chicken-outline" as const,
