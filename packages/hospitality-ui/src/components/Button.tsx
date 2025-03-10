dc269c3e3030885fae6395f88c69f94504887a6c
Mihailo <mihailo.dilparic@gmail.com>
2025-03-10 15:14:54 +0100

    feat: has no border button prop and styles

--- packages/hospitality-ui/src/components/Button.tsx
+++ packages/hospitality-ui/src/components/Button.tsx
@@ -10,6 +10,7 @@ type Props = {
   size?: Size;
   isOutline?: boolean;
   icon?: availableIcons;
+  hasNoBorder?: boolean;
   onClick: MouseEventHandler<HTMLButtonElement> | undefined;
 };
 
@@ -26,46 +27,89 @@ const classes = tv({
     },
     size: { xs: "h-6 text-xs", sm: "h-7 text-sm", md: "h-8", lg: "h-9 text-lg", xl: "h-10 text-xl" },
     isOutline: { true: "border-2 bg-transparent text-black", false: "" },
+    hasNoBorder: { true: "border-0 shadow-none hover:bg-transparent active:bg-transparent", false: "" },
   },
 
   compoundVariants: [
     {
       variant: "primary",
       isOutline: true,
+      hasNoBorder: false,
       class: "text-gray-900 hover:text-white active:text-white",
     },
     {
       variant: "secondary",
       isOutline: true,
+      hasNoBorder: false,
       class: "text-gray-600 hover:text-white active:text-white",
     },
     {
       variant: "info",
       isOutline: true,
+      hasNoBorder: false,
       class: "text-blue-600 hover:text-white active:text-white",
     },
     {
       variant: "success",
       isOutline: true,
+      hasNoBorder: false,
       class: "text-green-600 hover:text-white active:text-white",
     },
     {
       variant: "warning",
       isOutline: true,
+      hasNoBorder: false,
       class: "text-orange-600 hover:text-white active:text-white",
     },
     {
       variant: "error",
       isOutline: true,
+      hasNoBorder: false,
       class: "text-red-600 hover:text-white active:text-white",
+    },
+    {
+      variant: "primary",
+      isOutline: true,
+      hasNoBorder: true,
+      class: "text-gray-900",
+    },
+    {
+      variant: "secondary",
+      isOutline: true,
+      hasNoBorder: true,
+      class: "text-gray-600",
+    },
+    {
+      variant: "info",
+      isOutline: true,
+      hasNoBorder: true,
+      class: "text-blue-600",
+    },
+    {
+      variant: "success",
+      isOutline: true,
+      hasNoBorder: true,
+      class: "text-green-600",
+    },
+    {
+      variant: "warning",
+      isOutline: true,
+      hasNoBorder: true,
+      class: "text-orange-600",
+    },
+    {
+      variant: "error",
+      isOutline: true,
+      hasNoBorder: true,
+      class: "text-red-600",
     },
   ],
 });
 
-export function Button({ label, variant = "primary", size = "md", isOutline = false, icon, onClick }: Props) {
+export function Button({ label, variant = "primary", size = "md", isOutline = false, hasNoBorder, icon, onClick }: Props) {
   if (!label && !icon) return null;
   return (
-    <button className={classes({ variant, size, isOutline })} onClick={onClick}>
+    <button className={classes({ variant, size, isOutline, hasNoBorder })} onClick={onClick}>
       {label ? <div className="max-w-5/6 truncate">{label}</div> : null} {icon ? <Icon icon={icon} /> : null}
     </button>
   );
