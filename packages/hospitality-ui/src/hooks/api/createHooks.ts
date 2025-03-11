1be261e477ba65d460268612879b904d98a4c68f
Mihailo <mihailo.dilparic@gmail.com>
2025-03-11 14:07:58 +0100

    chore: update create hook

--- packages/hospitality-ui/src/hooks/api/createHooks.ts
+++ packages/hospitality-ui/src/hooks/api/createHooks.ts
@@ -2,17 +2,18 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useResetAtom } from "jotai/utils";
 
 import { userAtom } from "../../atoms";
-import { AvailableEntities, valueof } from "../../types";
+import { AvailableEntities } from "../../types";
 import { fetchFunction } from "../../utils";
 
-export function useCreate<F extends Record<keyof F, valueof<F>>>(model: AvailableEntities, payload: F) {
+export function useCreate<F>(model: AvailableEntities) {
   const userReset = useResetAtom(userAtom);
   const queryClient = useQueryClient();
+
   return useMutation({
-    mutationFn: () => fetchFunction({ method: "POST", payload: JSON.stringify(payload), model, userReset }),
-    meta: {
-      model,
+    mutationFn: (payload: { value: F }) => {
+      return fetchFunction({ method: "POST", payload: JSON.stringify(payload.value), model, userReset });
     },
+
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [model] });
     },
