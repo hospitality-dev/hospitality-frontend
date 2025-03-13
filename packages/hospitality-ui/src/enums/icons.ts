bd605d1120588a3090f2b8522f356ab0e49a0b22
Mihailo <mihailo.dilparic@gmail.com>
2025-03-13 16:51:25 +0100

    feat: barcode icon

--- packages/hospitality-ui/src/enums/icons.ts
+++ packages/hospitality-ui/src/enums/icons.ts
@@ -3,6 +3,7 @@ export const Icons = {
   "arrow-down": "ph:caret-down" as const,
   "arrow-right": "ph:caret-right" as const,
   "arrow-up": "ph:caret-up" as const,
+  barcode: "ph:barcode" as const,
   beer: "ph:beer-bottle-light" as const,
   chicken: "healthicons:animal-chicken-outline" as const,
   folder: "ph:folder" as const,
