f350a09b16156b7de2a5eb22ee100665eaadfefd
Mihailo <mihailo.dilparic@gmail.com>
2025-03-14 08:47:32 +0100

    feat: icon and position base types

--- packages/hospitality-ui/src/types/baseTypes.ts
+++ packages/hospitality-ui/src/types/baseTypes.ts
@@ -11,7 +11,32 @@ export type Size = "xs" | "sm" | "md" | "lg" | "xl";
 
 export type OptionType = { label: string; value: string };
 
+export type PositionType = (
+  | "top"
+  | "right"
+  | "bottom"
+  | "left"
+  | "right-start"
+  | "right-end"
+  | "left-start"
+  | "left-end"
+  | "bottom-start"
+  | "top-start"
+  | "top-end"
+  | "bottom-end"
+)[];
+
 export type availableIcons = valueof<typeof Icons>;
+export type IconThickness = "thin" | "light" | "regular" | "bold" | "fill";
+export interface IconType {
+  icon: availableIcons;
+  fontSize?: number;
+  color?: string;
+  hFlip?: boolean;
+  vFlip?: boolean;
+  className?: string;
+  thickness?: IconThickness;
+}
 
 // =========RESPONSE=========
 export type ResponseType<T> = { data: T; ok: boolean; message: string };
