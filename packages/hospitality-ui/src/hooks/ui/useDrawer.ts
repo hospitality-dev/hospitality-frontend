6a96e32c79e57ed8fbd62a4b5bc4e15e7bf756ff
Mihailo <mihailo.dilparic@gmail.com>
2025-03-17 16:26:43 +0100

    fix: use drawer fn

--- packages/hospitality-ui/src/hooks/ui/useDrawer.ts
+++ packages/hospitality-ui/src/hooks/ui/useDrawer.ts
@@ -1,15 +1,25 @@
 import { useAtom } from "jotai";
 import { useResetAtom } from "jotai/utils";
 
-import { drawerAtom } from "../../atoms";
-import { DrawerTypes } from "../../types";
+import { drawerAtom, DrawerTypes } from "../../atoms";
+
+type DrawerData<T extends DrawerTypes["type"]> = Extract<DrawerTypes, { type: T }>["data"];
 
 export function useDrawer<T extends DrawerTypes["type"]>(title: string, type: T) {
   const [drawer, setDrawer] = useAtom(drawerAtom);
   const closeDrawer = useResetAtom(drawerAtom);
 
-  function openDrawer(data?: DrawerTypes["data"] | null) {
-    setDrawer((prev) => ({ ...prev, title, isOpen: true, type, data }));
+  function openDrawer(data?: DrawerData<T>) {
+    if (data === null || type === null) return;
+
+    // @ts-expect-error TS can't differentiate the type well enough even though it's correct
+    setDrawer((prev) => ({
+      ...prev,
+      title,
+      type,
+      data,
+      isOpen: true,
+    }));
   }
 
   return { drawer, openDrawer, closeDrawer };
