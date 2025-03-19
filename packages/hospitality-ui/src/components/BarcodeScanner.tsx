c4528fe6a22c1c046c6bbfb6965d33bc0bf3f80f
Mihailo <mihailo.dilparic@gmail.com>
2025-03-19 12:05:19 +0100

    chore: reattempt zxing

--- packages/hospitality-ui/src/components/BarcodeScanner.tsx
+++ packages/hospitality-ui/src/components/BarcodeScanner.tsx
@@ -1,45 +1,49 @@
-import { BarcodeScanner as Scanner } from "@capacitor-mlkit/barcode-scanning";
-import { useEffect, useState } from "react";
-
-import { Select } from "./Select";
+import { BrowserMultiFormatReader } from "@zxing/library";
+import { useEffect, useRef, useState } from "react";
 
 export function BarcodeScanner() {
-  const [videoDevices] = useState<MediaDeviceInfo[]>([]);
-  const [videoDevice, setVideoDevice] = useState<string>();
-  // const closeBarcodeScanner = useResetAtom(barcodeScannerAtom);
-
-  // function close(e: globalThis.KeyboardEvent) {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  //   if (e.key === "Escape") closeBarcodeScanner();
-  // }
+  const videoRef = useRef(null);
+  const [barcode] = useState(null);
 
   useEffect(() => {
+    const codeReader = new BrowserMultiFormatReader();
+    let isScanning = true;
+
     async function startScanning() {
       try {
-        const result = await Scanner.scan();
-        if (result.barcodes) {
-          fetch("https://thearkive.requestcatcher.com/test", { method: "POST", body: JSON.stringify(result.barcodes) });
-        }
-      } catch (error) {
-        console.error("Error scanning barcode", error);
+        const videoInputDevices = await codeReader.listVideoInputDevices();
+        const selectedDeviceId = videoInputDevices[0].deviceId;
+
+        codeReader.decodeFromVideoDevice(selectedDeviceId, videoRef.current, (result, error) => {
+          if (result) {
+            fetch("https://thearkive.requestcatcher.com/test", { method: "POST", body: JSON.stringify(result.getText()) });
+          }
+          if (error && isScanning) {
+            console.error(error);
+          }
+        });
+      } catch (err) {
+        console.error("Error starting barcode scanner:", err);
       }
     }
+
     startScanning();
+
+    return () => {
+      isScanning = false;
+      codeReader.reset();
+    };
   }, []);
 
   return (
-    <div className="bg-layout absolute top-0 left-0 z-50 flex h-screen w-screen flex-col p-4">
-      <div className="relative flex h-full flex-col">
-        <Select
-          onChange={({ target }) => setVideoDevice(target.value)}
-          options={videoDevices.map((d) => ({ label: d.label, value: d.deviceId }))}
-          size="xl"
-          value={videoDevice || ""}
-        />
-        <div className="absolute top-1/2 z-10 h-0.5 w-full bg-red-500" />
-        <video height="200" id="barcode_scanner" width="300" />
-      </div>
+    <div>
+      <video ref={videoRef} height="auto" width="100%" />
+      {barcode && (
+        <div>
+          <h2>Scanned Barcode:</h2>
+          <p>{barcode}</p>
+        </div>
+      )}
     </div>
   );
 }
