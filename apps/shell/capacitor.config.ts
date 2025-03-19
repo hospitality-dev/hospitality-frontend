be57beff7814d5562992f750a7526ac35cd81b33
Mihailo <mihailo.dilparic@gmail.com>
2025-03-19 12:23:00 +0100

    chore: remove capacitor

--- apps/shell/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "app.thearkive.hospitality",
-  appName: "Hospitality",
-  webDir: "dist",
-};
-
-export default config;
