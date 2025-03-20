86057cfb48de253b7e180ee253dae8c5faf72a8e
Mihailo <mihailo.dilparic@gmail.com>
2025-03-20 16:49:52 +0100

    feat: queries

--- packages/hospitality-ui/src/utils/queries.ts
+++ packages/hospitality-ui/src/utils/queries.ts
@@ -1,6 +1,7 @@
-import { LocationsAvailableProducts, LocationsAvailableProductsSettings } from "../types";
+import { LocationsAvailableProducts, LocationsAvailableProductsSettings, ProductsCategories } from "../types";
 import { fetchFunction, getSearchParams } from "./request";
-const locationsAvailableProductsFields: (keyof LocationsAvailableProducts)[] = ["productId"];
+export const locationsAvailableProductsFields: (keyof LocationsAvailableProducts)[] = ["productId"];
+export const productCategoryFields: (keyof ProductsCategories)[] = ["id", "title", "companyId", "parentId", "isDefault"];
 
 export const LocationsAvailableProductsQuery = {
   queryKey: ["locations_available_products"],
@@ -10,6 +11,22 @@ export const LocationsAvailableProductsQuery = {
       searchParams: getSearchParams<typeof locationsAvailableProductsFields, null>(locationsAvailableProductsFields),
       userReset: () => {},
       model: "locations_available_products",
+      // * exceptions since this is neither reading a specific entity
+      // * nor is it listing
+      id: "00000000-0000-0000-0000-000000000000",
     }),
   staleTime: Infinity,
+};
+
+export const ProductCategoriesQuery = {
+  queryKey: ["products_categories", "list"],
+  queryFn: () =>
+    fetchFunction<ProductsCategories[]>({
+      method: "GET",
+      userReset: () => {},
+      searchParams: getSearchParams<typeof productCategoryFields, ProductsCategories>(productCategoryFields),
+      model: "products_categories",
+      urlSuffix: "list",
+    }),
+  staleTime: 5 * 60 * 1000,
 };
