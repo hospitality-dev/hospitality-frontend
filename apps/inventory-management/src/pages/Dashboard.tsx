de8132b738f1d7debae4d728dbc2016648f07fe6
Mihailo <mihailo.dilparic@gmail.com>
2025-03-20 13:40:40 +0100

    feat: dummy inventory dashboard

--- apps/inventory-management/src/pages/Dashboard.tsx
+++ apps/inventory-management/src/pages/Dashboard.tsx
@@ -1,3 +1,26 @@
+import { Card, Title } from "@hospitality/hospitality-ui";
+
 export function Dashboard() {
-  return <div>DASHBOARD</div>;
+  return (
+    <div className="flex flex-col gap-y-2">
+      <Card isFullWidth>
+        <div className="px-4 py-2">
+          <Title hasBorder label="Latest reservations" size="xl" variant="info" />
+          Work in progress...
+        </div>
+      </Card>
+      <Card isFullWidth>
+        <div className="px-4 py-2">
+          <Title hasBorder label="Critical inventory levels" size="xl" variant="error" />
+          Work in progress...
+        </div>
+      </Card>
+      <Card isFullWidth>
+        <div className="px-4 py-2">
+          <Title hasBorder label="Inventory levels" size="xl" variant="success" />
+          Work in progress...
+        </div>
+      </Card>
+    </div>
+  );
 }
