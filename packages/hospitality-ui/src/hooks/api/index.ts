3f147b50b6384db9423379912212f822119f821b
Mihailo <mihailo.dilparic@gmail.com>
2025-03-20 10:33:19 +0100

    feat: initial delete and update hooks

--- packages/hospitality-ui/src/hooks/api/index.ts
+++ packages/hospitality-ui/src/hooks/api/index.ts
@@ -1,4 +1,6 @@
 export * from "./authHooks";
 export * from "./createHooks";
+export * from "./deleteHooks";
 export * from "./listHooks";
 export * from "./readHooks";
+export * from "./updateHooks";
