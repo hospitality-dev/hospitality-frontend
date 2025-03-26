9bd5d599fb56ddde4a22a5229474cd8507eb56a3
Mihailo <mihailo.dilparic@gmail.com>
2025-03-26 12:33:55 +0100

    feat: title prop for modules

--- apps/shell/src/enums/modules.ts
+++ apps/shell/src/enums/modules.ts
@@ -1,17 +1,20 @@
 import { availableIcons, Icons } from "@hospitality/hospitality-ui";
 
-export const Modules: { id: string; icon: availableIcons }[] = [
+export const Modules: { id: string; title: string; icon: availableIcons }[] = [
   {
     id: "booking" as const,
+    title: "Booking",
     icon: Icons.booking,
   },
   {
     id: "employee-management" as const,
+    title: "Employee management" as const,
     icon: Icons.employee,
   },
-  { id: "inventory-management" as const, icon: Icons.inventory },
+  { id: "inventory-management/alcoholic drinks" as const, title: "Inventory management", icon: Icons.inventory },
   {
     id: "suppliers" as const,
+    title: "Suppliers",
     icon: Icons.supplier,
   },
 ];
