55e2ba29e3441edbf8a7a6dccbcc3048391c9aa9
Mihailo <mihailo.dilparic@gmail.com>
2025-03-27 13:14:00 +0100

    chore: change drawer types

--- packages/hospitality-ui/src/atoms/uiAtoms.ts
+++ packages/hospitality-ui/src/atoms/uiAtoms.ts
@@ -10,9 +10,9 @@ type SidebarState = {
 export type DrawerTypes =
   | { type: null; data: null }
   | { type: "products_categories"; data?: { id?: string } }
-  | { type: "products"; data: { id?: string; categoryId?: string; barcode?: string } }
+  | { type: "add_products"; data: { id?: string; categoryId?: string; barcode?: string } }
   | { type: "inventory_products"; data: { categoryId: string; barcode?: string } }
-  | { type: "remove_by_barcode"; data: { maxAmount: number; barcode: string } };
+  | { type: "remove_products"; data: { id?: string; maxAmount: number; barcode?: string } };
 
 type DrawerState = {
   isOpen: boolean;
