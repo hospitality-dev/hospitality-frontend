07fe595273b3c8beb43610cdcf780a61c436886c
Mihailo <mihailo.dilparic@gmail.com>
2025-03-27 10:40:22 +0100

    feat: new icon

--- packages/hospitality-ui/src/enums/icons.ts
+++ packages/hospitality-ui/src/enums/icons.ts
@@ -14,6 +14,7 @@ export const Icons = {
   input: "streamline:input-box" as const,
   inventory: "system-uicons:boxes" as const,
   loading: "ph:circle-notch" as const,
+  manage: "fluent:clipboard-text-edit-24-regular" as const,
   menu: "ph:dots-three-circle-thin" as const,
   settings: "ph:gear" as const,
   supplier: "ph:truck" as const,
