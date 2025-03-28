825834d979f804748f4c21ced2fd7a9668767ac5
Mihailo <mihailo.dilparic@gmail.com>
2025-03-28 12:05:13 +0100

    chore: split hospitality apps

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,16 @@ export default defineConfig({
       output: {
         minifyInternalExports: true,
         manualChunks(id) {
-          console.log(id);
           if (id.includes("@zxing")) return "@zxing";
           if (id.includes("lodash")) return "lodash";
           if (id.includes("@floating-ui")) return "@floating-ui";
+          // if (id.includes("hospitality-ui/inventory-managemety"))
           if (id.includes("hospitality-ui/src/hooks")) return "@hospitality-ui/hooks";
           if (id.includes("hospitality-ui/src/components")) return "@hospitality-ui/components";
           if (id.includes("hospitality-ui/src/sections")) return "@hospitality-ui/sections";
+          if (id.includes("hospitality-frontend/apps/")) {
+            return `@hospitality-${id.split("/")[6]}`;
+          }
           if (id.includes("@tanstack/react-form")) return "@tanstack/react-form";
           if (id.includes("@tanstack/react-query")) return "@tanstack/react-query";
           if (id.includes("@tanstack/react-table")) return "@tanstack/react-table";
