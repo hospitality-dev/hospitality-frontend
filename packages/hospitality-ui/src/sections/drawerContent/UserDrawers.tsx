cfbb9ed16b8d265d0ee3f3f6f695cefa8b7f75ee
Mihailo <mihailo.dilparic@gmail.com>
2025-03-31 09:34:47 +0200

    chore: remove fragment

--- packages/hospitality-ui/src/sections/drawerContent/UserDrawers.tsx
+++ packages/hospitality-ui/src/sections/drawerContent/UserDrawers.tsx
@@ -12,57 +12,55 @@ export function AddNewUser() {
   });
 
   return (
-    <>
-      <Form handleSubmit={form.handleSubmit}>
-        <div className="grid h-full grid-cols-1 content-start gap-2 md:grid-cols-2">
-          <form.Field
-            children={(field) => (
-              <Input
-                helperText={formatErrorsForHelperText(field.state.meta.errors)}
-                label="First name"
-                name={field.name}
-                onChange={(e) => field.handleChange(e.target.value)}
-                value={field.state.value}
-              />
-            )}
-            name="firstName"
-          />
-          <form.Field
-            children={(field) => (
-              <Input
-                helperText={formatErrorsForHelperText(field.state.meta.errors)}
-                label="Last name"
-                name={field.name}
-                onChange={(e) => field.handleChange(e.target.value)}
-                value={field.state.value}
-              />
-            )}
-            name="lastName"
-          />
-          <div className="md:col-span-2">
-            <form.Field
-              children={(field) => (
-                <Input
-                  helperText={formatErrorsForHelperText(field.state.meta.errors) || "Must be at least 6 characters long"}
-                  label="Username"
-                  name={field.name}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  value={field.state.value}
-                />
-              )}
-              name="username"
+    <Form handleSubmit={form.handleSubmit}>
+      <div className="grid h-full grid-cols-1 content-start gap-2 md:grid-cols-2">
+        <form.Field
+          children={(field) => (
+            <Input
+              helperText={formatErrorsForHelperText(field.state.meta.errors)}
+              label="First name"
+              name={field.name}
+              onChange={(e) => field.handleChange(e.target.value)}
+              value={field.state.value}
             />
-          </div>
-        </div>
-        <div className="mt-auto">
-          <form.Subscribe<[boolean, boolean]>
-            children={(p) => {
-              return <Button isDisabled={!p[0]} label="Create" onClick={undefined} variant="success" />;
-            }}
-            selector={(state) => [state.canSubmit, state.isSubmitting]}
+          )}
+          name="firstName"
+        />
+        <form.Field
+          children={(field) => (
+            <Input
+              helperText={formatErrorsForHelperText(field.state.meta.errors)}
+              label="Last name"
+              name={field.name}
+              onChange={(e) => field.handleChange(e.target.value)}
+              value={field.state.value}
+            />
+          )}
+          name="lastName"
+        />
+        <div className="md:col-span-2">
+          <form.Field
+            children={(field) => (
+              <Input
+                helperText={formatErrorsForHelperText(field.state.meta.errors) || "Must be at least 6 characters long"}
+                label="Username"
+                name={field.name}
+                onChange={(e) => field.handleChange(e.target.value)}
+                value={field.state.value}
+              />
+            )}
+            name="username"
           />
         </div>
-      </Form>
-    </>
+      </div>
+      <div className="mt-auto">
+        <form.Subscribe<[boolean, boolean]>
+          children={(p) => {
+            return <Button isDisabled={!p[0]} label="Create" onClick={undefined} variant="success" />;
+          }}
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+        />
+      </div>
+    </Form>
   );
 }
