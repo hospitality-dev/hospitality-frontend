cfb7f2cb27e84a053bf34841e6599bfd39890b59
Mihailo <mihailo.dilparic@gmail.com>
2025-03-31 09:35:44 +0200

    feat: lazy load drawers

--- apps/shell/src/components/Layout.tsx
+++ apps/shell/src/components/Layout.tsx
@@ -1,6 +1,38 @@
-import { BarcodeScanner, Drawer, Navbar, Outlet, Sidebar, useBarcodeScanner, useScreenSize } from "@hospitality/hospitality-ui";
+import {
+  BarcodeScanner,
+  Drawer,
+  Navbar,
+  Outlet,
+  Sidebar,
+  Spinner,
+  useBarcodeScanner,
+  useDrawerValue,
+  useScreenSize,
+} from "@hospitality/hospitality-ui";
+import { lazy, Suspense } from "react";
 
 import { Modules } from "../enums";
+
+const ProductsCategories = lazy(() =>
+  import("@hospitality/hospitality-ui").then((module) => ({
+    default: module.ProductsCategoriesDrawer,
+  }))
+);
+const CreateProduct = lazy(() =>
+  import("@hospitality/hospitality-ui").then((module) => ({
+    default: module.CreateProduct,
+  }))
+);
+const ManageProductInventory = lazy(() =>
+  import("@hospitality/hospitality-ui").then((module) => ({
+    default: module.ManageProductInventory,
+  }))
+);
+const AddNewUser = lazy(() =>
+  import("@hospitality/hospitality-ui").then((module) => ({
+    default: module.AddNewUser,
+  }))
+);
 
 const sections = [
   {
@@ -12,9 +44,22 @@ const sections = [
 export function Layout() {
   const { scannerState } = useBarcodeScanner();
   const { isLg } = useScreenSize();
+  const drawer = useDrawerValue();
   return (
     <main className="bg-layout relative flex h-screen w-screen flex-nowrap overflow-hidden">
-      <Drawer />
+      <Drawer>
+        <Suspense
+          fallback={
+            <div className="flex h-full w-full items-center justify-center">
+              <Spinner />
+            </div>
+          }>
+          {drawer.type === "products_categories" ? <ProductsCategories /> : null}
+          {drawer.type === "create_products" && drawer.data ? <CreateProduct data={drawer.data} /> : null}
+          {drawer.type === "manage_product_inventory" && drawer.data ? <ManageProductInventory data={drawer.data} /> : null}
+          {drawer.type === "add_new_user" ? <AddNewUser /> : null}
+        </Suspense>
+      </Drawer>
       {scannerState.isOpen ? <BarcodeScanner /> : null}
       {isLg ? <Sidebar sections={sections} /> : null}
       <div className="flex w-full flex-col">
