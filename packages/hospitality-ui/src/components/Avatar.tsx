0ad169f4f24fa559624fb42d2df77835941cadf3
sergio <srdjanljiljak10@gmail.com>
2025-03-31 19:42:10 +0200

    feat: initial avatar component

---
+++ packages/hospitality-ui/src/components/Avatar.tsx
@@ -0,0 +1,56 @@
+import { tv } from "tailwind-variants";
+
+import { Size, Variant } from "../types/baseTypes";
+
+type Props = {
+  label?: string;
+  variant?: Variant;
+  size?: Size;
+  imageId?: string;
+};
+
+const classes = tv({
+  base: "bg-secondary flex aspect-square cursor-pointer items-center justify-center overflow-hidden rounded-full border-2 font-medium text-white shadow select-none",
+  variants: {
+    variant: {
+      primary: "border-transparent",
+      secondary: "border-transparent",
+      info: "border-info",
+      success: "border-success",
+      warning: "border-warning",
+      error: "border-error",
+    },
+    size: {
+      xs: "h-10 w-10 text-xs",
+      sm: "h-11 w-11 text-sm",
+      md: "h-12 w-12 text-sm",
+      lg: "h-13 w-13",
+      xl: "h-14 w-14 text-xl",
+    },
+  },
+});
+
+function getInitials(name: Props["label"]) {
+  if (!name) {
+    return "";
+  }
+  const parts = name.split(" ");
+  let initials = "";
+  for (let i = 0; i < parts.length; i++) {
+    if (parts[i].length > 0 && parts[i] !== "") {
+      initials += parts[i][0];
+    }
+    if (i === 2) {
+      break;
+    }
+  }
+  return initials;
+}
+
+export function Avatar({ label, variant = "error", size = "xl", imageId }: Props) {
+  return (
+    <div className={classes({ variant, size })}>
+      {imageId ? <img className="h-full w-full object-cover" src={imageId} /> : getInitials(label)}
+    </div>
+  );
+}
