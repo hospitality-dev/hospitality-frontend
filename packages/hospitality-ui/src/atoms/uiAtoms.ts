7439aa26100fec7bef5204a39e6e7b3a193e556b
Mihailo <mihailo.dilparic@gmail.com>
2025-04-03 11:55:51 +0200

    feat: drawer type

--- packages/hospitality-ui/src/atoms/uiAtoms.ts
+++ packages/hospitality-ui/src/atoms/uiAtoms.ts
@@ -22,6 +22,10 @@ export type DrawerTypes = (
       type: "add_new_user";
       data: null;
     }
+  | {
+      type: "add_user_from_location";
+      data: null;
+    }
 ) & { title: string };
 
 type DrawerState = {
