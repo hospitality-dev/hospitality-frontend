97c74525fa7d51cdf817b908ce52c005cd9fed3d
Mihailo <mihailo.dilparic@gmail.com>
2025-04-03 11:56:01 +0200

    chore: change generic

--- packages/hospitality-ui/src/hooks/api/listHooks.ts
+++ packages/hospitality-ui/src/hooks/api/listHooks.ts
@@ -2,7 +2,7 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { useResetAtom } from "jotai/utils";
 
 import { userAtom } from "../../atoms";
-import { AvailableEntities, RequestFilters, valueof } from "../../types";
+import { AvailableEntities, RequestFilters } from "../../types";
 import { fetchFunction, getSearchParams } from "../../utils";
 
 export type useListProps<F> = {
@@ -12,7 +12,7 @@ export type useListProps<F> = {
   placeholderData?: F[];
 };
 
-export function useList<F extends Record<keyof F, valueof<F>>>(
+export function useList<F>(
   { model, fields, filters, placeholderData = [] }: useListProps<F>,
   options?: Pick<UseQueryOptions<F>, "enabled" | "placeholderData"> & { urlSuffix?: string; searchParams?: string[][] }
 ) {
