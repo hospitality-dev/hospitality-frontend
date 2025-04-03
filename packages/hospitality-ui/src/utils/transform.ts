c3864617fb05a170b7c11e07a341839d87946a05
Mihailo <mihailo.dilparic@gmail.com>
2025-04-03 11:49:55 +0200

    feat: initial format user info fn

--- packages/hospitality-ui/src/utils/transform.ts
+++ packages/hospitality-ui/src/utils/transform.ts
@@ -1,17 +1,27 @@
 import { ValidationError } from "@tanstack/react-form";
 
-import { OptionType } from "../types";
+import { OptionType, UsersType } from "../types";
 
 export function getSentenceCase(field: string) {
   const text = field?.replaceAll(/[_-]/g, " ")?.replace(/([A-Z])/g, " $1") || "";
   return `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
 }
 
-export function formatForOptions<T extends { id: string; title: string }>(data?: T[]): OptionType[] {
+export function formatForOptions<T extends { id: string; title: string }>(data?: T[], isSentenceCase?: boolean): OptionType[] {
   if (!data) return [];
-  return data.map((item) => ({ label: item.title, value: item.id }));
+  return data.map((item) => ({
+    label: isSentenceCase === true || isSentenceCase === undefined ? getSentenceCase(item.title) : item.title,
+    value: item.id,
+  }));
 }
 
 export function formatErrorsForHelperText(errors: ValidationError[]) {
   return errors.join("\n ");
+}
+
+export function getUserInfo(user: UsersType) {
+  return {
+    id: user.id,
+    title: `${user.firstName} ${user.lastName}`,
+  };
 }
