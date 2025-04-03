02579226733e8e068c56f85572cea6c8075dffc8
Mihailo <mihailo.dilparic@gmail.com>
2025-04-03 11:59:19 +0200

    chore: change default avatar size

--- packages/hospitality-ui/src/components/Avatar.tsx
+++ packages/hospitality-ui/src/components/Avatar.tsx
@@ -47,7 +47,7 @@ function getInitials(name: Props["label"]) {
   return initials;
 }
 
-export function Avatar({ label, variant = "error", size = "xl", imageId }: Props) {
+export function Avatar({ label, variant = "error", size = "md", imageId }: Props) {
   return (
     <div className={classes({ variant, size })}>
       {imageId ? <img className="h-full w-full object-cover" src={imageId} /> : getInitials(label)}
