463435582cce2bc9b9808017edc1fd399402e3c9
Mihailo <mihailo.dilparic@gmail.com>
2025-04-05 19:12:32 +0200

    chore: update use search hook types

--- packages/hospitality-ui/src/hooks/api/searchHooks.ts
+++ packages/hospitality-ui/src/hooks/api/searchHooks.ts
@@ -1,14 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { useResetAtom } from "jotai/utils";
 
 import { userAtom } from "../../atoms";
 import { AvailableSearchableEntities } from "../../types";
 import { searchFunction } from "../../utils";
 
-export function useSearch<F>({ model, searchQuery }: { model: AvailableSearchableEntities; searchQuery: string }) {
+export function useSearch<F>(
+  { model, searchQuery }: { model: AvailableSearchableEntities; searchQuery: string },
+  options?: Pick<UseQueryOptions<F>, "enabled">
+) {
   const userReset = useResetAtom(userAtom);
   return useQuery({
     queryKey: [model, "search", searchQuery],
     queryFn: () => searchFunction<F>({ model, searchQuery, userReset }),
+    enabled: options?.enabled === undefined || !!options?.enabled,
   });
 }
