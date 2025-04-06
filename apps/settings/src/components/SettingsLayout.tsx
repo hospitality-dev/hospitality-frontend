cdf93d599ef2a2e50f9caa1775bcd720dd956e2e
Mihailo <mihailo.dilparic@gmail.com>
2025-04-06 19:44:48 +0200

    chore: adjust height

--- apps/settings/src/components/SettingsLayout.tsx
+++ apps/settings/src/components/SettingsLayout.tsx
@@ -8,9 +8,9 @@ const tabs = [
 export function SettingsLayout() {
   const location = useLocation();
   return (
-    <div className="flex flex-col gap-y-2 overflow-hidden">
+    <div className="flex h-full flex-col gap-y-2 overflow-hidden">
       <Tabs active={location.href.split("/").at(-1) || tabs[0]?.id} isNavControlled setActive={() => {}} tabs={tabs} />
-      <div className="max-h-full overflow-y-auto">
+      <div className="h-full max-h-full overflow-y-auto">
         <Outlet />
       </div>
     </div>
