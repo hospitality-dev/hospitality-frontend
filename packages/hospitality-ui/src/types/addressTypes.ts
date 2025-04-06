41c928767c13fef7e3ca24a08f9ab4799df9ef9e
Mihailo <mihailo.dilparic@gmail.com>
2025-04-06 08:43:44 +0200

    fix: addresses type

--- packages/hospitality-ui/src/types/addressTypes.ts
+++ packages/hospitality-ui/src/types/addressTypes.ts
@@ -1,34 +1,32 @@
 import { infer as zodInfer, number, object, record, string } from "zod";
 
 export const AddressesSchema = object({
-  features: object({
-    osmType: string(),
-    placeId: number(),
-    lat: number(),
-    lng: number(),
+  osmType: string(),
+  placeId: number(),
+  lat: number(),
+  lng: number(),
+  type: string(),
+  displayName: string(),
+  name: string(),
+  extratags: record(string(), string()).nullable(),
+  namedetails: record(string(), string()).nullable(),
+  boundingbox: number().array(),
+  address: object({
+    houseNumber: string(),
+    road: string(),
+    neighbourhood: string(),
+    suburb: string(),
+    city: string(),
+    county: string(),
+    state: string(),
+    postcode: string(),
+    country: string(),
+    countryCode: string(),
+  }),
+  geometry: object({
     type: string(),
-    displayName: string(),
-    name: string(),
-    extratags: record(string(), string()).nullable(),
-    namedetails: record(string(), string()).nullable(),
-    boundingbox: number().array(),
-    address: object({
-      houseNumber: string(),
-      road: string(),
-      neighbourhood: string(),
-      suburb: string(),
-      city: string(),
-      county: string(),
-      state: string(),
-      postcode: string(),
-      country: string(),
-      countryCode: string(),
-    }),
-    geometry: object({
-      type: string(),
-      coordinates: number().array().length(2),
-    }),
-  }).array(),
+    coordinates: number().array().length(2),
+  }),
 });
 
 export type AddressesType = zodInfer<typeof AddressesSchema>;
