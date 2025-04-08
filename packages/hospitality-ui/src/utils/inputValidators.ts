9128f443caedd066f1b7bd41b4f93a57601c09ad
Mihailo <mihailo.dilparic@gmail.com>
2025-04-08 12:50:55 +0200

    feat: url validation

--- packages/hospitality-ui/src/utils/inputValidators.ts
+++ packages/hospitality-ui/src/utils/inputValidators.ts
@@ -9,3 +9,17 @@ export function emailValidation(f: { value: unknown }): string | undefined {
   }
   return undefined;
 }
+
+export function urlValidation(f: { value: unknown }): string | undefined {
+  const res = string()
+    .regex(/^https:\/\/([\w-]+\.)+[\w-]+$/, "Please enter a valid URL.")
+    .regex(
+      /\.(com|net|app|org|co|io|xyz|co.uk|co.ca|co.us|rs|срб|)$/,
+      "Only .com .net .app .org .co .io .xyz .co.uk .co.ca .co.us .rs .срб are allowed as top level domains."
+    )
+    .safeParse(f.value);
+  if (!res.success) {
+    return formatErrorsForHelperText(res.error.errors.map((i) => i.message));
+  }
+  return undefined;
+}
