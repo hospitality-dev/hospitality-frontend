51d5dc4f9dbda6c51f2acb54d8fdaa2f86b5d701
Mihailo <mihailo.dilparic@gmail.com>
2025-04-10 17:07:27 +0200

    fix: response hook when text

--- packages/hospitality-ui/src/utils/response.ts
+++ packages/hospitality-ui/src/utils/response.ts
@@ -5,20 +5,26 @@ import { ResponseType } from "../types";
 
 // =========== HOOKS ===========
 export async function formatDataResponseHook<R>(
-  _: KyRequest,
-  __: NormalizedOptions,
+  r: KyRequest,
+  _: NormalizedOptions,
   response: KyResponse,
   reset: () => void
 ): Promise<Response | void> {
-  const res = await response.json<ResponseType<R>>();
-  if (res.ok) return new Response(JSON.stringify(res));
-  else {
-    if (response.status === 401) {
-      // TODO: HIT LOGOUT ROUTE
-      localStorage.setItem("user_id", "");
-      reset();
-      redirect({ to: "/" });
-    }
+  if (response.status === 401) {
+    // TODO: HIT LOGOUT ROUTE
+    localStorage.setItem("user_id", "");
+    reset();
+    redirect({ to: "/" });
+    return;
+  }
+  if (r.headers.get("Content-Type") === "application/json") {
+    const res = await response.json<ResponseType<R>>();
+    if (response.ok && res.ok) return new Response(JSON.stringify(res));
+    return new Response(JSON.stringify({ data: {}, ok: false, message: "There was an error with your request." }));
+  } else if (r.headers.get("Content-Type") === "text/plain") {
+    const res = await response.text();
+    if (response.ok) return new Response(res);
+    return new Response(JSON.stringify({ data: {}, ok: false, message: "There was an error with your request." }));
   }
 }
 export function rejectOnUnauthorized() {}
