e90e6337524ad7fb5625c4a5b0fc18eaea6ed98d
Mihailo <mihailo.dilparic@gmail.com>
2025-04-10 11:31:48 +0200

    feat: initial upload component functionality

--- packages/hospitality-ui/src/components/Upload.tsx
+++ packages/hospitality-ui/src/components/Upload.tsx
@@ -1,18 +1,21 @@
-import { MutableRefObject, useRef } from "react";
+import { ChangeEventHandler, MutableRefObject, useRef } from "react";
 import { tv } from "tailwind-variants";
 
 import { Icons } from "../enums";
+import { AllowedFileTypes } from "../types";
 import { Icon } from "./Icon";
 
 type Props = {
   label: string;
   isDisabled?: boolean;
-  onChange: () => void;
+  isMultiple?: boolean;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  types: AllowedFileTypes[];
 };
 
 const classes = tv({
   slots: {
-    base: "flex h-56 items-center justify-center rounded border border-gray-300 bg-gray-100 p-4 shadow",
+    base: "flex h-56 items-center justify-center rounded-md border border-gray-400 bg-gray-50 p-4 shadow",
     titleClasses: "flex flex-col items-center justify-center gap-y-4 text-3xl text-gray-400",
   },
   variants: {
@@ -23,7 +26,7 @@ const classes = tv({
   },
 });
 
-export function Upload({ label, isDisabled }: Props) {
+export function Upload({ label, isDisabled, isMultiple, onChange, types }: Props) {
   const inputRef = useRef() as MutableRefObject<HTMLInputElement>;
   const { base, titleClasses } = classes({ isDisabled });
 
@@ -35,7 +38,16 @@ export function Upload({ label, isDisabled }: Props) {
           <span>Click here or drag files to upload</span>
           <Icon fontSize={56} icon={Icons.upload} />
         </h3>
-        <input ref={inputRef} className="hidden" id="upload" multiple name="upload" type="file" />
+        <input
+          ref={inputRef}
+          accept={types.map((t) => `.${t}`).join(", ")}
+          className="hidden"
+          id="upload"
+          multiple={isMultiple}
+          name="upload"
+          onChange={onChange}
+          type="file"
+        />
       </div>
     </div>
   );
