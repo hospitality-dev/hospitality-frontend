64d7988324688abbc1b69b1c1c745f1590c6e0a7
Mihailo <mihailo.dilparic@gmail.com>
2025-04-11 16:40:03 +0200

    feat: user avatar profile

--- packages/hospitality-ui/src/components/Navbar.tsx
+++ packages/hospitality-ui/src/components/Navbar.tsx
@@ -2,20 +2,28 @@ import { useNavigate } from "@tanstack/react-router";
 import { MouseEvent } from "react";
 
 import { Icons } from "../enums";
-import { useAuth, useUserLocations } from "../hooks";
+import { useAuth, useRead, useUserLocations } from "../hooks";
+import { UsersType } from "../types";
 import { getLoginRoute, getUserInfo } from "../utils";
 import { Avatar } from "./Avatar";
 import { Dropdown } from "./Dropdown";
-
 export function Navbar() {
   const auth = useAuth();
   const { data: locations } = useUserLocations();
-  const name = auth.user ? getUserInfo(auth.user).title : "";
   const navigate = useNavigate();
   function routeToLogin(e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>) {
     e.preventDefault();
     getLoginRoute();
   }
+
+  const { data: userData } = useRead<UsersType>(
+    {
+      model: "users",
+      id: auth.user?.id || "",
+      fields: ["id", "firstName", "lastName", "imageId"],
+    },
+    { enabled: !!auth.user?.id, staleTime: Infinity }
+  );
 
   return (
     <div className="h-16 min-h-16 w-full bg-gray-100 px-4 shadow">
@@ -33,7 +41,7 @@ export function Navbar() {
                     onClick: () => navigate({ to: "/location-select" }),
                   },
                 ]}>
-                <Avatar label={name} onClick={() => {}} />
+                <Avatar imageId={userData?.imageId} label={getUserInfo(userData).title} onClick={() => {}} type="user_avatar" />
               </Dropdown>
             ) : (
               <a href="#" onClick={routeToLogin}>
