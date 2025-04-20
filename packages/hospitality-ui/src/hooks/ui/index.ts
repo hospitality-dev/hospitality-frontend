dd36f30501d78e9fc5480f59483183cb30fcb0f3
Mihailo <mihailo.dilparic@gmail.com>
2025-04-20 16:44:02 +0200

    chore: export hooks

--- packages/hospitality-ui/src/hooks/ui/index.ts
+++ packages/hospitality-ui/src/hooks/ui/index.ts
@@ -2,5 +2,6 @@ export * from "./useBarcodeScanner";
 export * from "./useClickOutside";
 export * from "./useDrawer";
 export * from "./useRole";
+export * from "./useRole";
 export * from "./useScreenSize";
 export * from "./useTable";
