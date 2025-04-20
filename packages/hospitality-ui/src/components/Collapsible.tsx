4108cb1cba0529f00cbf9000344a29fdc3f78c64
Mihailo <mihailo.dilparic@gmail.com>
2025-04-20 17:42:57 +0200

    feat: animation for collapsible

--- packages/hospitality-ui/src/components/Collapsible.tsx
+++ packages/hospitality-ui/src/components/Collapsible.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useLayoutEffect, useState } from "react";
+import { tv } from "tailwind-variants";
 
 import { Icons } from "../enums";
 import { ActionType, AvailableIcons, Size, Variant } from "../types";
@@ -14,7 +15,15 @@ type Props = {
   size?: Size;
   items?: ActionType[];
 };
-
+const classes = tv({
+  base: "min-w-full overflow-auto transition-[height]",
+  variants: {
+    isOpen: {
+      true: "overflow-hidden",
+      false: "h-0 overflow-hidden",
+    },
+  },
+});
 export function Collapsible({ icon, label, children, isOpen, isInitialOpen, items, variant = "primary", size = "md" }: Props) {
   const [isExpanded, setIsExpanded] = useState(!!isOpen || !!isInitialOpen);
 
@@ -23,36 +32,37 @@ export function Collapsible({ icon, label, children, isOpen, isInitialOpen, item
   }, [isOpen]);
 
   return (
-    <details open={isExpanded}>
-      <summary
-        className="cursor-pointer p-0.5"
-        onClick={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          setIsExpanded((prev) => !prev);
-        }}>
-        <Title
-          hasBorder={isExpanded}
-          icon={icon}
-          items={[
-            ...(isExpanded ? items || [] : []),
-            {
-              id: "collapse",
-              icon: Icons[isExpanded ? "arrowUp" : "arrowDown"],
-              onClick: (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                setIsExpanded((prev) => !prev);
-              },
-              label: "",
+    <div className="flex-col">
+      <Title
+        hasBorder={isExpanded}
+        icon={icon}
+        items={[
+          ...(isExpanded ? items || [] : []),
+          {
+            id: "collapse",
+            icon: Icons[isExpanded ? "arrowUp" : "arrowDown"],
+            onClick: (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              setIsExpanded((prev) => !prev);
             },
-          ]}
-          label={label}
-          size={size}
-          variant={variant}
-        />
-      </summary>
-      {isExpanded ? children : null}
-    </details>
+            label: "",
+          },
+        ]}
+        label={label}
+        size={size}
+        variant={variant}
+      />
+      <div className={classes({ isOpen: isExpanded })} style={{ height: isExpanded ? "calc-size(auto, size)" : 0 }}>
+        <div
+          className="cursor-pointer p-0.5"
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setIsExpanded((prev) => !prev);
+          }}></div>
+        {children}
+      </div>
+    </div>
   );
 }
