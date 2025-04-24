fe43127654d1ce4c5e77fb414ede4f4e8c6a5ae3
Mihailo <mihailo.dilparic@gmail.com>
2025-04-24 08:08:40 +0200

    feat: suppliers types

---
+++ packages/hospitality-ui/src/types/supplierTypes.ts
@@ -0,0 +1,26 @@
+import { boolean, infer as zodInfer, object, string } from "zod";
+
+import { ContactSchema } from "./contactTypes";
+
+export const SuppliersSchema = object({
+  id: string().uuid().nonempty(),
+  title: string().nonempty(),
+  ownerId: string().uuid().nonempty(),
+  companyId: string().uuid().nonempty(),
+  isDefault: boolean().default(false),
+  contacts: ContactSchema.array().default([]),
+});
+
+export const SuppliersInitializerSchema = object({
+  title: string().nonempty(),
+  contacts: ContactSchema.array().default([]),
+});
+
+export const SuppliersMutatorSchema = object({
+  title: string().optional(),
+  contacts: ContactSchema.array().optional(),
+});
+
+export type SuppliersType = zodInfer<typeof SuppliersSchema>;
+export type SuppliersInitalizerType = zodInfer<typeof SuppliersInitializerSchema>;
+export type SuppliersMutatorType = zodInfer<typeof SuppliersMutatorSchema>;
