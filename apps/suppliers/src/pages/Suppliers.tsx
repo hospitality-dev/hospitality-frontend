d097f362a7ffae9fc1952ebecf0a6f611d8b4d94
Mihailo <mihailo.dilparic@gmail.com>
2025-04-29 12:56:50 +0200

    feat: initial supplier actions

--- apps/suppliers/src/pages/Suppliers.tsx
+++ apps/suppliers/src/pages/Suppliers.tsx
@@ -1,7 +1,78 @@
-import { Button, createColumnHelper, Icons, SuppliersType, Table, useList, useTable } from "@hospitality/hospitality-ui";
+import {
+  Button,
+  CellContext,
+  ContactType,
+  createColumnHelper,
+  groupByContacts,
+  Icons,
+  SuppliersType,
+  Table,
+  useList,
+  useTable,
+} from "@hospitality/hospitality-ui";
+import { useState } from "react";
 
-type Entity = Pick<SuppliersType, "id" | "title">;
+type Entity = Pick<SuppliersType, "id" | "title" | "companyId">;
 const columnHelper = createColumnHelper<Entity>();
+
+function ActionButton(info: CellContext<Entity, unknown>) {
+  const [isActive, setIsActive] = useState(false);
+  const { data: contacts = [] } = useList<ContactType>(
+    { model: "suppliers", fields: ["id", "title", "value", "contactType"] },
+    { urlSuffix: `supplier/${info.row.original.id}`, enabled: isActive }
+  );
+  const groupedContacts = groupByContacts(contacts);
+  return (
+    <div
+      onMouseOver={() => {
+        if (!isActive) setIsActive(true);
+      }}>
+      <Button
+        allowedPlacements={["left", "left-start", "left-end"]}
+        hasNoBorder
+        icon={Icons.menu}
+        isOutline
+        items={[
+          {
+            id: "contact",
+            title: "Contact supplier",
+            icon: Icons.phone,
+            allowedPlacements: ["left-start"],
+            subItems: [
+              {
+                id: "phone",
+                title: "Phone numbers",
+                icon: Icons.phone,
+                subItems: groupedContacts.phone.map((c) => ({ id: c.id, title: `${c.prefix}${c.value}` })),
+              },
+              {
+                id: "phone",
+                title: "Email",
+                icon: Icons.email,
+                subItems: groupedContacts.email.map((c) => ({ id: c.id, title: c.value })),
+              },
+              {
+                id: "websites",
+                title: "Websites",
+                icon: Icons.website,
+                subItems: groupedContacts.website.map((c) => ({ id: c.id, title: c.value })),
+              },
+              {
+                id: "other",
+                title: "Other",
+                icon: Icons.info,
+                subItems: groupedContacts.other.map((c) => ({ id: c.id, title: c.value })),
+              },
+            ],
+          },
+          { id: "edit", title: "Edit supplier", icon: Icons.edit, isHidden: !info.row.original.companyId },
+        ]}
+        onClick={undefined}
+        size="xl"
+      />
+    </div>
+  );
+}
 
 const columns = [
   columnHelper.accessor("title", {
@@ -25,7 +96,7 @@ const columns = [
   columnHelper.display({
     id: "isActive",
     header: "Actions",
-    cell: () => <Button hasNoBorder icon={Icons.menu} isOutline onClick={() => {}} size="xl" />,
+    cell: ActionButton,
     minSize: 100,
     maxSize: 120,
     size: 120,
@@ -36,7 +107,7 @@ const columns = [
 ];
 
 export function Suppliers() {
-  const { data: suppliers = [] } = useList<SuppliersType>({ model: "suppliers", fields: ["id", "title"] });
+  const { data: suppliers = [] } = useList<SuppliersType>({ model: "suppliers", fields: ["id", "title", "companyId"] });
   const [meta, dispatch] = useTable<Entity>();
   return (
     <div className="flex flex-col gap-y-2">
