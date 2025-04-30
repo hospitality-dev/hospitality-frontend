2757a4bc9f444dac854abf3f19de55112216b81a
Mihailo <mihailo.dilparic@gmail.com>
2025-04-30 10:35:52 +0200

    feat: format search results automatically

--- packages/hospitality-ui/src/hooks/api/searchHooks.ts
+++ packages/hospitality-ui/src/hooks/api/searchHooks.ts
@@ -2,17 +2,18 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { useResetAtom } from "jotai/utils";
 
 import { userAtom } from "../../atoms";
-import { AvailableSearchableEntities } from "../../types";
-import { searchFunction } from "../../utils";
+import { AvailableSearchableEntities, OptionType } from "../../types";
+import { formatForOptions, searchFunction } from "../../utils";
 
-export function useSearch<F>(
+export function useSearch<F extends { id: string; title: string }>(
   { model, searchQuery }: { model: AvailableSearchableEntities; searchQuery: string },
-  options?: Pick<UseQueryOptions<F>, "enabled">
+  options?: Pick<UseQueryOptions<F>, "enabled" | "select">
 ) {
   const userReset = useResetAtom(userAtom);
-  return useQuery<F[]>({
+  return useQuery<F[], unknown, OptionType[]>({
     queryKey: [model, "search", searchQuery],
     queryFn: () => searchFunction<F[]>({ model, searchQuery, userReset }),
     enabled: options?.enabled === undefined || !!options?.enabled,
+    select: (data) => formatForOptions(data, false),
   });
 }
